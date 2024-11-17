06137e4a28859a8229ab050a1f662c7a7af57cd6
Adrian Schneider <adrian.schneider11@gmail.com>
2024-11-17 13:07:41 +0100

    chore(backend): delete collection api related code

--- backend/src/core/mod.rs
+++ backend/src/core/mod.rs
@@ -1,10 +1,9 @@
 mod best_combination_handler;
-mod collection_handler;
-mod collection_service;
 mod health_handler;
 mod routes;
 mod team_handler;
 mod team_mapper;
 mod team_service;
+mod tests;
 
 pub use routes::configure_routes;
--- backend/src/core/routes.rs
+++ backend/src/core/routes.rs
@@ -1,11 +1,7 @@
-use super::{
-    collection_handler::get_all_collection_names, health_handler::health,
-    team_handler::get_all_team_names,
-};
+use super::{health_handler::health, team_handler::get_all_team_names};
 use actix_web::web;
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(health);
-    cfg.service(get_all_collection_names);
     cfg.service(get_all_team_names);
 }
