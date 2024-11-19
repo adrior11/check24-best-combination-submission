521f1ac6a67e47e5e8df77b22e82b87d140866c1
Adrian Schneider <adrian.schneider11@gmail.com>
2024-11-19 13:41:06 +0100

    refactor(metrics): adjust label names in MetricLabelName enum

--- backend/src/libs/metrics/counters.rs
+++ backend/src/libs/metrics/counters.rs
@@ -1,12 +1,32 @@
 use once_cell::sync;
 
+enum MetricLabelName {
+    Method,
+    Endpoint,
+    Status,
+}
+
+impl MetricLabelName {
+    fn as_str(&self) -> &'static str {
+        match self {
+            MetricLabelName::Method => "method",
+            MetricLabelName::Endpoint => "endpoint",
+            MetricLabelName::Status => "status",
+        }
+    }
+}
+
 pub static REQUEST_DURATION: sync::Lazy<prometheus::HistogramVec> = sync::Lazy::new(|| {
     prometheus::HistogramVec::new(
         prometheus::HistogramOpts::new(
             "api_request_duration_seconds",
             "Histogram for the duration of API requests in seconds",
         ),
-        &["method", "endpoint", "status"],
+        &[
+            MetricLabelName::Method.as_str(),
+            MetricLabelName::Endpoint.as_str(),
+            MetricLabelName::Status.as_str(),
+        ],
     )
     .expect("Failed to create REQUEST_DURATION")
 });
@@ -14,7 +34,11 @@ pub static REQUEST_DURATION: sync::Lazy<prometheus::HistogramVec> = sync::Lazy::
 pub static REQUEST_COUNT: sync::Lazy<prometheus::IntCounterVec> = sync::Lazy::new(|| {
     prometheus::IntCounterVec::new(
         prometheus::Opts::new("api_requests_total", "Total number of API requests"),
-        &["method", "endpoint", "status"],
+        &[
+            MetricLabelName::Method.as_str(),
+            MetricLabelName::Endpoint.as_str(),
+            MetricLabelName::Status.as_str(),
+        ],
     )
     .expect("Failed to create REQUEST_COUNT")
 });
