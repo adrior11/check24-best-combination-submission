1dcb9ca6ee170fa36e6b602f8ef008173f12d029
Adrian Schneider <adrian.schneider11@gmail.com>
2024-12-15 16:41:02 +0100

    feat(mongodb): add additional index for bc_streaming_offer

--- mongodb/mongo_init.sh
+++ mongodb/mongo_init.sh
@@ -54,6 +54,7 @@ function ensureIndex(collectionName, indexSpec, indexName) {
 
 ensureIndex("bc_game", { team_home: 1, team_away: 1 }, "team_home_1_team_away_1");
 ensureIndex("bc_streaming_offer", { game_id: 1 }, "game_id_1");
+ensureIndex("bc_streaming_offer", { streaming_package_id: 1 }, "streaming_package_id_1");
 ensureIndex("bc_streaming_package", { streaming_package_id: 1 }, "streaming_package_id_1");
 EOF
 
