c99075a73be3013eb4052e1b82d96ed9bc615a22
Adrian Schneider <adrian.schneider11@gmail.com>
2024-12-22 20:55:44 +0100

    feat(libs): add aggregate_game_ids to game dao for dynamic user input

--- apps/libs/src/db/dao/game_dao.rs
+++ apps/libs/src/db/dao/game_dao.rs
@@ -1,9 +1,9 @@
-use crate::models::schemas::GameSchema;
 use anyhow::Context;
 use futures::stream::TryStreamExt;
 use mongodb::{bson, Collection};
 
-use super::pipelines;
+use super::{pipelines, util};
+use crate::models::schemas::GameSchema;
 
 pub struct GameDao {
     collection: Collection<GameSchema>,
@@ -14,27 +14,31 @@ impl GameDao {
         Self { collection }
     }
 
-    pub async fn fetch_games(&self, teams: &[String]) -> anyhow::Result<Vec<GameSchema>> {
-        let filter = bson::doc! {
-            "$or": [
-                { "team_home": { "$in": teams } },
-                { "team_away": { "$in": teams } },
-            ]
-        };
+    // TODO: Add find games by tournaments & teams
+    // TODO: Add get all teams, tournaments
 
-        let cursor = self.collection.find(filter).await?;
-        let games = cursor.try_collect().await?;
+    pub async fn aggregate_game_ids(
+        &self,
+        teams: Option<Vec<String>>,
+        tournaments: Option<Vec<String>>,
+    ) -> anyhow::Result<Vec<usize>> {
+        let match_query = util::build_match_query(teams, tournaments);
 
-        Ok(games)
-    }
+        // Return an empty array, if no input has been provided
+        if match_query.is_none() {
+            return Ok(vec![]);
+        }
 
-    pub async fn fetch_game_ids(&self, teams: &[String]) -> anyhow::Result<Vec<u32>> {
-        let pipeline = pipelines::games_by_teams_pipeline(teams);
+        let pipeline = vec![
+            bson::doc! { "$match": match_query.unwrap() },
+            pipelines::project_game_id(),
+        ];
+
         let mut cursor = self.collection.aggregate(pipeline).await?;
         let mut game_ids = Vec::new();
 
         while let Some(doc) = cursor.try_next().await? {
-            if let Ok(game_id) = bson::from_bson::<u32>(
+            if let Ok(game_id) = bson::from_bson::<usize>(
                 doc.get("game_id")
                     .context("Failed to parse game_id")?
                     .clone(),
@@ -44,5 +48,113 @@ impl GameDao {
         }
 
         Ok(game_ids)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        constants::{DATABASE_NAME, GAME_COLLECTION_NAME},
+        db::{DocumentDatabaseConnector, MongoClient},
+    };
+    use std::env;
+
+    #[tokio::test]
+    async fn test_aggregate_game_ids_without_input() {
+        dotenv::dotenv().ok();
+        let uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set in env");
+        let mongo_client = MongoClient::init(&uri, DATABASE_NAME).await;
+        let game_dao = GameDao::new(mongo_client.get_collection(GAME_COLLECTION_NAME));
+
+        let game_ids = game_dao.aggregate_game_ids(None, None).await.unwrap();
+        assert!(game_ids.is_empty());
+    }
+
+    #[tokio::test]
+    async fn test_aggregate_game_ids_by_teams() {
+        dotenv::dotenv().ok();
+        let uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set in env");
+        let mongo_client = MongoClient::init(&uri, DATABASE_NAME).await;
+        let game_dao = GameDao::new(mongo_client.get_collection(GAME_COLLECTION_NAME));
+
+        let teams = vec!["Bayern München".to_string()];
+        let mut game_ids = game_dao
+            .aggregate_game_ids(Some(teams), None)
+            .await
+            .unwrap();
+        let mut expected = vec![
+            52, 69, 76, 79, 103, 89, 113, 121, 125, 139, 146, 151, 161, 171, 186, 193, 196, 212,
+            214, 219, 225, 240, 251, 257, 261, 272, 284, 293, 307, 320, 302, 325, 337, 349, 356,
+            5305, 5320, 5325, 5330, 5341, 5349, 5364, 5367, 5383, 5386, 5394, 5404, 5416, 5436,
+            5440, 5422, 5449, 5459, 5467, 5474, 5483, 5492, 5501, 5511, 5525, 5529, 5541, 5548,
+            5557, 5566, 5584, 5573, 5593, 7354, 7890, 8440, 8466, 8486, 8514, 8503, 8533, 8568,
+            8560, 8845,
+        ];
+
+        game_ids.sort();
+        expected.sort();
+
+        assert!(!game_ids.is_empty());
+        assert_eq!(game_ids.len(), expected.len());
+        assert_eq!(game_ids, expected);
+    }
+
+    #[tokio::test]
+    async fn test_aggregate_game_ids_by_tournaments() {
+        dotenv::dotenv().ok();
+        let uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set in env");
+        let mongo_client = MongoClient::init(&uri, DATABASE_NAME).await;
+        let game_dao = GameDao::new(mongo_client.get_collection(GAME_COLLECTION_NAME));
+
+        let tournaments = vec!["Europameisterschaft 2024".to_string()];
+        let mut game_ids = game_dao
+            .aggregate_game_ids(None, Some(tournaments))
+            .await
+            .unwrap();
+        let mut expected = vec![
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+            25, 26, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 27, 48,
+            49, 50, 51, 29, 28,
+        ];
+
+        game_ids.sort();
+        expected.sort();
+
+        assert!(!game_ids.is_empty());
+        assert_eq!(game_ids.len(), expected.len());
+        assert_eq!(game_ids, expected);
+    }
+
+    #[tokio::test]
+    async fn test_aggregate_game_ids_by_teams_and_tournaments() {
+        dotenv::dotenv().ok();
+        let uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set in env");
+        let mongo_client = MongoClient::init(&uri, DATABASE_NAME).await;
+        let game_dao = GameDao::new(mongo_client.get_collection(GAME_COLLECTION_NAME));
+
+        let teams = vec!["Bayern München".to_string()];
+        let tournaments = vec!["Europameisterschaft 2024".to_string()];
+        let mut game_ids = game_dao
+            .aggregate_game_ids(Some(teams), Some(tournaments))
+            .await
+            .unwrap();
+        let mut expected = vec![
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+            25, 26, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 27, 48,
+            49, 50, 51, 52, 29, 28, 76, 79, 69, 89, 103, 113, 125, 121, 139, 146, 151, 161, 171,
+            186, 193, 196, 212, 214, 219, 225, 240, 257, 261, 251, 272, 284, 293, 302, 307, 320,
+            325, 337, 349, 356, 5305, 5320, 5325, 5330, 5341, 5349, 5364, 5367, 5386, 5394, 5383,
+            5404, 5416, 5422, 5436, 5449, 5440, 5459, 5467, 5474, 5483, 5492, 5501, 5511, 5525,
+            5529, 5541, 5548, 5557, 5566, 5573, 5584, 5593, 7354, 7890, 8440, 8466, 8486, 8503,
+            8514, 8533, 8560, 8568, 8845,
+        ];
+
+        game_ids.sort();
+        expected.sort();
+
+        assert!(!game_ids.is_empty());
+        assert_eq!(game_ids.len(), expected.len());
+        assert_eq!(game_ids, expected);
     }
 }
--- apps/libs/src/db/dao/pipelines.rs
+++ apps/libs/src/db/dao/pipelines.rs
@@ -1,42 +1,99 @@
 use mongodb::bson::{doc, Document};
 
-/// Creates an aggregation pipeline to filter games by teams and project the game_id.
-///
-/// # Arguments
-///
-/// * `teams` - A slice of team names to filter `team_home` or `team_away`.
-///
-/// # Returns
-///
-/// A vector of `Document` representing the aggregation pipeline.
-pub fn games_by_teams_pipeline(teams: &[String]) -> Vec<Document> {
-    vec![
-        doc! {
-            "$match": {
-                "$or": [
-                    { "team_home": { "$in": teams } },
-                    { "team_away": { "$in": teams } }
-                ]
-            }
-        },
-        doc! {
-            "$project": {
-                "_id": 0,
-                "game_id": 1
-            }
-        },
-    ]
+pub fn project_game_id() -> Document {
+    doc! { "$project": { "game_id": 1, "_id": 0 } }
 }
 
-/// Creates an aggregation pipeline for preprocessing subsets with filtered game_ids.
-///
-/// # Arguments
-///
-/// * `game_ids` - A slice of u32 representing the game_ids to filter.
-///
-/// # Returns
-///
-/// A vector of `Document` representing the aggregation pipeline.
+//pub fn best_combination_coverage(game_ids: &[u32], package_ids: &[u32]) -> Vec<Document> {
+//    vec![
+//        doc! {
+//            "game_id": {
+//                "$in": game_ids
+//            },
+//        },
+//        doc! {
+//            "$lookup": doc! {
+//                "from": "bc_streaming_offer",
+//                "localField": "game_id",
+//                "foreignField": "game_id",
+//                "as": "offers",
+//                "pipeline": [
+//                    doc! {
+//                        "$match": doc! {
+//                            "streaming_package_id": doc! {
+//                                "$in":  package_ids
+//                            }
+//                        }
+//                    }
+//                ]
+//            }
+//        },
+//        doc! {
+//            "$unwind": doc! {
+//                "path": "$offers"
+//            }
+//        },
+//        doc! {
+//            "$group": doc! {
+//                "_id": doc! {
+//                    "tournament_name": "$tournament_name",
+//                    "streaming_package_id": "$offers.streaming_package_id"
+//                },
+//                "total_games": doc! {
+//                    "$sum": 1
+//                },
+//                "live_coverage": doc! {
+//                    "$sum": doc! {
+//                        "$cond": [
+//                            "$offers.live",
+//                            1,
+//                            0
+//                        ]
+//                    }
+//                },
+//                "highlights_coverage": doc! {
+//                    "$sum": doc! {
+//                        "$cond": [
+//                            "$offers.highlights",
+//                            1,
+//                            0
+//                        ]
+//                    }
+//                }
+//            }
+//        },
+//        doc! {
+//            "$project": doc! {
+//                "_id": 0,
+//                "tournament_name": "$_id.tournament_name",
+//                "streaming_package_id": "$_id.streaming_package_id",
+//                "total_games": 1,
+//                "live_coverage": 1,
+//                "highlights_coverage": 1
+//            }
+//        },
+//    ]
+//}
+//
+//pub fn games_by_teams_pipeline(teams: &[String]) -> Vec<Document> {
+//    vec![
+//        doc! {
+//            "$match": {
+//                "$or": [
+//                    { "team_home": { "$in": teams } },
+//                    { "team_away": { "$in": teams } }
+//                ]
+//            }
+//        },
+//        doc! {
+//            "$project": {
+//                "_id": 0,
+//                "game_id": 1
+//            }
+//        },
+//    ]
+//}
+
 pub fn preprocess_subsets_pipeline(game_ids: &[u32]) -> Vec<Document> {
     vec![
         doc! {
@@ -54,9 +111,29 @@ pub fn preprocess_subsets_pipeline(game_ids: &[u32]) -> Vec<Document> {
                         }
                     },
                     doc! {
+                        "$lookup": doc! {
+                            "from": "bc_game",
+                            "localField": "game_id",
+                            "foreignField": "game_id",
+                            "as": "game",
+                            "pipeline": [
+                                doc! {
+                                    "$project": doc! {
+                                        "_id": 0,
+                                        "tournament_name": 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    doc! {
+                        "$unwind": "$game"
+                    },
+                    doc! {
                         "$project": doc! {
                             "_id": 0,
-                            "game_id": 1
+                            "game_id": 1,
+                            "tournament_name": "$game.tournament_name"
                         }
                     }
                 ]
@@ -72,26 +149,17 @@ pub fn preprocess_subsets_pipeline(game_ids: &[u32]) -> Vec<Document> {
         doc! {
             "$project": doc! {
                 "_id": 0,
-                "id": "$streaming_package_id",
-                "cost": doc! {
-                    "$cond": doc! {
-                        "if": doc! {
-                            "$eq": [
-                                "$monthly_price_cents",
-                                ""
-                            ]
-                        },
-                        "then": 100000,
-                        "else": doc! {
-                            "$toInt": "$monthly_price_cents"
-                        }
-                    }
-                },
+                "streaming_package_id": 1,
+                "monthly_price_cents": 1,
+                "monthly_price_yearly_subscription_in_cents": 1,
                 "elements": doc! {
                     "$map": doc! {
                         "input": "$offers",
                         "as": "offer",
-                        "in": "$$offer.game_id"
+                        "in": [
+                            "$$offer.game_id",
+                            "$$offer.tournament_name"
+                        ]
                     }
                 }
             }
